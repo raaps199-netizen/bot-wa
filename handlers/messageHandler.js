2b6e9128bbe3e49a801d63dfe2eb87a3bf813a45
raaps199-netizen <raaps199@gmail.com>
2026-09-10 14:50:57 +0700

    Add new command handlers to messageHandler

--- handlers/messageHandler.js
+++ handlers/messageHandler.js
@@ -13,6 +13,19 @@ const quoteCommand = require('../commands/quote');
 const rvoCommand = require('../commands/rvo');
 const handleJadwalCommand = require('../commands/jadwal');
 
+// Fitur Baru
+const aiCommand = require('../commands/ai');
+const hdCommand = require('../commands/hd');
+const sswebCommand = require('../commands/ssweb');
+const playCommand = require('../commands/play');
+const ytmp3Command = require('../commands/ytmp3');
+const cekkhodamCommand = require('../commands/cekkhodam');
+const tebakgambarCommand = require('../commands/tebakgambar');
+const truthCommand = require('../commands/truth');
+const dareCommand = require('../commands/dare');
+const cekbucinCommand = require('../commands/cekbucin');
+const tovidCommand = require('../commands/tovid');
+
 async function handleMessage(sock, msg) {
   try {
     const messageContent = msg.message;
@@ -34,6 +47,146 @@ async function handleMessage(sock, msg) {
       case 's':
       case 'sticker':
         await stickerCommand(sock, msg);
+        break;
+
+      case 'tt':
+      case 'tiktok':
+        await tiktokCommand(sock, msg, args);
+        break;
+
+      case 'ig':
+      case 'instagram':
+        await igCommand(sock, msg, args);
+        break;
+
+      case 'brat':
+        await bratCommand(sock, msg, args);
+        break;
+
+      case 'bratvid':
+        await bratvidCommand(sock, msg, args);
+        break;
+
+      case 'wm':
+        await wmCommand(sock, msg, args);
+        break;
+
+      case 'hidetag':
+      case 'h':
+        await hidetagCommand(sock, msg, args);
+        break;
+
+      case 'close':
+      case 'tutup':
+        await groupCommand(sock, msg, args, 'close');
+        break;
+
+      case 'open':
+      case 'buka':
+        await groupCommand(sock, msg, args, 'open');
+        break;
+
+      case 'promote':
+      case 'pm':
+        await groupCommand(sock, msg, args, 'promote');
+        break;
+
+      case 'demote':
+      case 'dm':
+        await groupCommand(sock, msg, args, 'demote');
+        break;
+
+      case 'toimg':
+        await toimgCommand(sock, msg);
+        break;
+
+      case 'tovid':
+      case 'tomp4':
+        await tovidCommand(sock, msg);
+        break;
+
+      case 'quote':
+      case 'q':
+      case 'qc':
+        await quoteCommand(sock, msg, args);
+        break;
+
+      case 'rvo':
+      case 'viewonce':
+      case 'save':
+        await rvoCommand(sock, msg);
+        break;
+
+      case 'ai':
+      case 'tanya':
+        await aiCommand(sock, msg, args);
+        break;
+
+      case 'hd':
+      case 'remini':
+      case 'enhance':
+        await hdCommand(sock, msg);
+        break;
+
+      case 'ss':
+      case 'ssweb':
+        await sswebCommand(sock, msg, args);
+        break;
+
+      case 'play':
+        await playCommand(sock, msg, args);
+        break;
+
+      case 'ytmp3':
+      case 'yta':
+        await ytmp3Command(sock, msg, args);
+        break;
+
+      case 'cekkhodam':
+      case 'khodam':
+        await cekkhodamCommand(sock, msg, args);
+        break;
+
+      case 'tebakgambar':
+        await tebakgambarCommand(sock, msg);
+        break;
+
+      case 'truth':
+        await truthCommand(sock, msg);
+        break;
+
+      case 'dare':
+        await dareCommand(sock, msg);
+        break;
+
+      case 'cekbucin':
+      case 'bucin':
+        await cekbucinCommand(sock, msg, args);
+        break;
+
+      case 'jsn':
+      case 'jsl':
+      case 'jrb':
+      case 'jkm':
+      case 'jjt':
+        await handleJadwalCommand(sock, msg, command);
+        break;
+
+      case 'list':
+      case 'menu':
+      case 'help':
+        await listCommand(sock, msg);
+        break;
+
+      default:
+        break;
+    }
+  } catch (err) {
+    console.error('Error di messageHandler:', err);
+  }
+}
+
+module.exports = handleMessage;
         break;
 
       case 'tt':
