002f273471731bc2373029ce54c75cd12a4612ec
raaps199-netizen <raaps199@gmail.com>
2026-09-11 16:26:01 +0700

    Add geminiKey to config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   prefix: '.',
   botName: 'Bot WA',
-  ownerNumbers: ['6289531307627']
+  ownerNumbers: ['6289531307627'],
+  geminiKey: 'AQ.Ab8RN6LHSKWPUlwKHxuZZ8H17f0KTMm2fgR8tkyX49PnmYjB8w'
 };
