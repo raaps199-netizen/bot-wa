b9d8dc09beaa9f60fff7272584ed3cebd31cdb34
raaps199-netizen <raaps199@gmail.com>
2026-09-11 19:55:12 +0700

    Add score command to retrieve user scores

---
+++ commands/score.js
@@ -0,0 +1,23 @@
+async function scoreCommand(sock, msg, args) {
+  const remoteJid = msg.key.remoteJid;
+  const senderId = msg.key.participant || remoteJid;
+
+  if (!global.db) global.db = {};
+  if (!global.db.users) global.db.users = {};
+
+  const userData = global.db.users[senderId] || { mathScore: 0, triviaScore: 0, name: msg.pushName || 'User' };
+  const totalScore = (userData.mathScore || 0) + (userData.triviaScore || 0);
+
+  const text = 
+`📊 *SKOR KAMU*
+
+👤 Nama: *${userData.name}*
+🧮 Skor Math: *${userData.mathScore || 0}*
+🧠 Skor Trivia: *${userData.triviaScore || 0}*
+🏆 Total Skor: *${totalScore}*`;
+
+  await sock.sendMessage(remoteJid, { text }, { quoted: msg });
+}
+
+module.exports = scoreCommand;
+
