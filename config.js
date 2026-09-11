f5b900c0c801ae48d45e6f02e5f049738472affe
raaps199-netizen <raaps199@gmail.com>
2026-09-11 17:58:48 +0700

    Replace geminiKey with groqKey in config.js

--- config.js
+++ config.js
@@ -2,5 +2,4 @@ module.exports = {
   prefix: '.',
   botName: 'Bot WA',
   ownerNumbers: ['6289531307627'],
-  geminiKey: 'AQ.Ab8RN6JuMqHc10eWQKI1nqM5JFYUbE-6_L_QXTQmZ_0I6orGfw'
-};
+  groqKey: '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'
