29f63a8dacf0956a4789984712de68ac17d1d773
raaps199-netizen <raaps199@gmail.com>
2026-09-11 17:47:38 +0700

    Change geminiKey in config.js
    Updated geminiKey to a new value.

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   prefix: '.',
   botName: 'Bot WA',
   ownerNumbers: ['6289531307627'],
-  geminiKey: 'AQ.Ab8RN6LHSKWPUlwKHxuZZ8H17f0KTMm2fgR8tkyX49PnmYjB8w'
+  geminiKey: 'AQ.Ab8RN6JuMqHc10eWQKI1nqM5JFYUbE-6_L_QXTQmZ_0I6orGfw'
 };
