360701bfa28fab14412d5e80b6eee62a657a7742
raaps199-netizen <raaps199@gmail.com>
2026-09-12 20:30:09 +0700

    Refactor leaderboard score calculation
    Refactor leaderboard command to use getTotalScore function for total score calculation and remove gambling score from leaderboard data.

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,3 +1,5 @@
+const { getTotalScore } = require('../utils/helper');
+
 async function leaderboardCommand(sock, msg) {
   const remoteJid = msg.key.remoteJid;
 
@@ -16,12 +18,10 @@ async function leaderboardCommand(sock, msg) {
     let leaderboardData = Object.entries(global.db.users).map(([jid, data]) => {
       const math = data.mathScore || 0;
       const trivia = data.triviaScore || 0;
-      const gambling = data.score || 0;
-      const total = math + trivia + gambling;
-      
+      const total = getTotalScore(data);
       const name = data.nickname || data.name || jid.split('@')[0];
 
-      return { jid, name, math, trivia, gambling, total };
+      return { jid, name, math, trivia, total };
     });
 
     leaderboardData = leaderboardData.filter(user => user.total > 0 || participantJids.has(user.jid));
@@ -32,7 +32,6 @@ async function leaderboardCommand(sock, msg) {
         name: p.id.split('@')[0],
         math: 0,
         trivia: 0,
-        gambling: 0,
         total: 0
       }));
     }
@@ -43,7 +42,7 @@ async function leaderboardCommand(sock, msg) {
     leaderboardData.slice(0, 10).forEach((user, index) => {
       const medal = index === 0 ? '🥇' : index === 1 ? '🥈' : index === 2 ? '🥉' : `${index + 1}.`;
       text += `${medal} *${user.name}*\n`;
-      text += `   └ Math: ${user.math} | Trivia: ${user.trivia} | Judi: ${user.gambling} | Total: *${user.total}*\n\n`;
+      text += `   └ Math: ${user.math} | Trivia: ${user.trivia} | Total: *${user.total}*\n\n`;
     });
 
     await sock.sendMessage(remoteJid, { text }, { quoted: msg });
