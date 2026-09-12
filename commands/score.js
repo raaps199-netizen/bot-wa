19bf4f6ec5efedccae6033d2bca9d962a5adf42d
raaps199-netizen <raaps199@gmail.com>
2026-09-12 20:29:39 +0700

    Refactor scoreCommand to use getUserData and getTotalScore

--- commands/score.js
+++ commands/score.js
@@ -1,3 +1,5 @@
+const { getUserData, getTotalScore } = require('../utils/helper');
+
 async function scoreCommand(sock, msg, args) {
   const remoteJid = msg.key.remoteJid;
   const senderId = msg.key.participant || remoteJid;
@@ -5,24 +7,22 @@ async function scoreCommand(sock, msg, args) {
   if (!global.db) global.db = {};
   if (!global.db.users) global.db.users = {};
 
-  const userData = global.db.users[senderId] || { mathScore: 0, triviaScore: 0, score: 0 };
-  const math = userData.mathScore || 0;
-  const trivia = userData.triviaScore || 0;
-  const gambling = userData.score || 0;
-  const totalScore = math + trivia + gambling;
-  
-  const displayName = userData.nickname || userData.name || msg.pushName || senderId.split('@')[0];
+  try {
+    const user = getUserData(global.db, senderId);
+    const math = user.mathScore || 0;
+    const trivia = user.triviaScore || 0;
+    const totalScore = getTotalScore(user);
+    
+    const displayName = user.nickname || user.name || msg.pushName || senderId.split('@')[0];
 
-  const text = 
+    const text = 
 `📊 *SKOR KAMU*
 
 👤 Nama: *${displayName}*
 🧮 Skor Math: *${math}*
 🧠 Skor Trivia: *${trivia}*
-🎲 Saldo Judi: *${gambling}*
 🏆 Total Skor: *${totalScore}*`;
 
-  try {
     await sock.sendMessage(remoteJid, { text }, { quoted: msg });
   } catch (err) {
     console.error('Error di scoreCommand:', err);
