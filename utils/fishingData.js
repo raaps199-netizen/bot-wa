c1d11467f6ea0a52ee74ed5cca00489b8d956066
raaps199-netizen <raaps199@gmail.com>
2026-09-14 23:53:15 +0700

    Update fishingData.js

--- utils/fishingData.js
+++ utils/fishingData.js
@@ -11,67 +11,73 @@ const rarityEmoji = {
 };
 
 const baits = {
-  roti: { name: '🍞 Roti', price: 50, luckBonus: 1 },
-  cacing: { name: '🪱 Cacing', price: 150, luckBonus: 1.2 },
-  pelet: { name: '🟢 Pelet Super', price: 350, luckBonus: 1.5 },
-  udang: { name: '🦐 Udang Segar', price: 750, luckBonus: 2.0 },
-  cumi: { name: '🦑 Cumi Impor', price: 1500, luckBonus: 3.0 }
+  roti: { name: '🍞 Roti', price: 100, luckBonus: 1 },
+  cacing: { name: '🪱 Cacing', price: 500, luckBonus: 1.2 },
+  pelet: { name: '🟢 Pelet Super', price: 1000, luckBonus: 1.5 },
+  udang: { name: '🦐 Udang Segar', price: 3000, luckBonus: 2.0 },
+  cumi: { name: '🦑 Cumi Impor', price: 5000, luckBonus: 4.0 }
 };
 
 const potions = {
-  minor: { name: '🧪 Minor Luck Potion', price: 1000, multiplier: 1.25, duration: 15 * 60 * 1000 },
-  major: { name: '🧪 Major Luck Potion', price: 2500, multiplier: 1.5, duration: 30 * 60 * 1000 },
-  divine: { name: '🧪 Divine Luck Potion', price: 6000, multiplier: 2.0, duration: 60 * 60 * 1000 }
+  minor: { name: '🧪 Minor Luck Potion', price: 20000, multiplier: 1.25, duration: 15 * 60 * 1000 },
+  major: { name: '🧪 Major Luck Potion', price: 50000, multiplier: 1.5, duration: 30 * 60 * 1000 },
+  divine: { name: '🧪 Divine Luck Potion', price: 100000, multiplier: 3.0, duration: 60 * 60 * 1000 }
 };
 
+// Total 10 Joran: 5 Joran awal (Timer 10-6s) & 5 Joran Sultan (Timer 5-1s)
 const rods = {
-  training: { name: '🎣 Training Rod', price: 0, luck: 1.0, timer: 5 },
-  fiber: { name: '🎣 Fiberglass Rod', price: 5000, luck: 1.3, timer: 4 },
-  carbon: { name: '🎣 Carbon Rod', price: 15000, luck: 1.7, timer: 3 },
-  magical: { name: '🎣 Mystic Wand Rod', price: 50000, luck: 2.3, timer: 2 },
-  crystal: { name: '🔱 Destiny Crystal Rod', price: 150000, luck: 3.5, timer: 1 }
+  training: { name: '🎣 Training Rod', price: 0, luck: 1.0, timer: 10 },
+  bamboo: { name: '🎣 Bamboo Rod', price: 10000, luck: 1.2, timer: 9 },
+  fiber: { name: '🎣 Fiberglass Rod', price: 35000, luck: 1.4, timer: 8 },
+  iron: { name: '🎣 Iron Reinforced Rod', price: 80000, luck: 1.6, timer: 7 },
+  carbon: { name: '🎣 Carbon Rod', price: 180000, luck: 1.9, timer: 6 },
+  magical: { name: '🎣 Mystic Wand Rod', price: 450000, luck: 2.5, timer: 5 },
+  crystal: { name: '🔱 Destiny Crystal Rod', price: 1500000, luck: 4.0, timer: 3 },
+  plasma: { name: '⚡ Plasma Core Rod', price: 6000000, luck: 7.0, timer: 2 },
+  abyssal: { name: '🌊 Abyssal Titan Rod', price: 25000000, luck: 14.0, timer: 1 },
+  quantum: { name: '🌌 Quantum Singularity Rod', price: 100000000, luck: 35.0, timer: 1 }
 };
 
 const fishPool = {
   COMMON: [
-    { name: 'Ikan Lele', price: 20 },
-    { name: 'Ikan Mujair', price: 25 },
-    { name: 'Ikan Nila', price: 30 },
-    { name: 'Ikan Mas', price: 35 },
-    { name: 'Ikan Sepat', price: 15 }
+    { name: 'Ikan Lele', price: 150 },
+    { name: 'Ikan Mujair', price: 200 },
+    { name: 'Ikan Nila', price: 250 },
+    { name: 'Ikan Mas', price: 300 },
+    { name: 'Ikan Sepat', price: 100 }
   ],
   UNCOMMON: [
-    { name: 'Ikan Gurame', price: 80 },
-    { name: 'Ikan Bawal', price: 100 },
-    { name: 'Ikan Patin', price: 120 },
-    { name: 'Ikan Gabus', price: 150 }
+    { name: 'Ikan Gurame', price: 750 },
+    { name: 'Ikan Bawal', price: 1000 },
+    { name: 'Ikan Patin', price: 1250 },
+    { name: 'Ikan Gabus', price: 1500 }
   ],
   RARE: [
-    { name: 'Ikan Salmon', price: 400 },
-    { name: 'Ikan Tuna', price: 500 },
-    { name: 'Ikan Kakap Merah', price: 650 },
-    { name: 'Ikan Tenggiri', price: 800 }
+    { name: 'Ikan Salmon', price: 4000 },
+    { name: 'Ikan Tuna', price: 5500 },
+    { name: 'Ikan Kakap Merah', price: 7000 },
+    { name: 'Ikan Tenggiri', price: 9000 }
   ],
   EPIC: [
-    { name: 'Ikan Hiu Martil', price: 2000 },
-    { name: 'Ikan Pari Manta', price: 2500 },
-    { name: 'Ikan Swordfish', price: 3200 },
-    { name: 'Ikan Marlin Biru', price: 4000 }
+    { name: 'Ikan Hiu Martil', price: 25000 },
+    { name: 'Ikan Pari Manta', price: 35000 },
+    { name: 'Ikan Swordfish', price: 45000 },
+    { name: 'Ikan Marlin Biru', price: 60000 }
   ],
   LEGENDARY: [
-    { name: 'Ikan Hiu Putih', price: 10000 },
-    { name: 'Ikan Paus Biru', price: 15000 },
-    { name: 'Ikan Coelacanth Purba', price: 22000 }
+    { name: 'Ikan Hiu Putih', price: 150000 },
+    { name: 'Ikan Paus Biru', price: 250000 },
+    { name: 'Ikan Coelacanth Purba', price: 400000 }
   ],
   MYTHIC: [
-    { name: 'Kraken Junior', price: 50000 },
-    { name: 'Naga Laut Dalam', price: 75000 },
-    { name: 'Leviathan Samudra', price: 100000 }
+    { name: 'Kraken Junior', price: 1000000 },
+    { name: 'Naga Laut Dalam', price: 1750000 },
+    { name: 'Leviathan Samudra', price: 2500000 }
   ],
   SECRET: [
-    { name: 'The Celestial Leviathan', price: 250000 },
-    { name: 'The Void Sovereign', price: 500000 },
-    { name: 'The Eternal Neptune', price: 1000000 }
+    { name: 'The Celestial Leviathan', price: 10000000 },
+    { name: 'The Void Sovereign', price: 25000000 },
+    { name: 'The Eternal Neptune', price: 50000000 }
   ]
 };
 
@@ -85,16 +91,13 @@ const mutations = [
 function getRandomCatch(potionBuff = 'normal', rodId = 'training') {
   let totalLuck = 1.0;
 
-  // 1. Rod Multiplier
   const rod = rods[rodId] || rods.training;
   totalLuck *= rod.luck;
 
-  // 2. Potion Multiplier
   if (potionBuff && potions[potionBuff]) {
     totalLuck *= potions[potionBuff].multiplier;
   }
 
-  // 3. Server Luck Multiplier (Diatur via .setluck)
   if (global.serverAuroraEvent && global.serverAuroraEvent.active) {
     const serverLuck = global.serverAuroraEvent.multiplier || 1.0;
     totalLuck *= serverLuck;
@@ -103,17 +106,18 @@ function getRandomCatch(potionBuff = 'normal', rodId = 'training') {
   const roll = Math.random() * 100;
 
   let rarity = 'COMMON';
-  if (roll < 0.5 * (totalLuck / 1.5)) {
+  
+  if (roll < 0.02 * totalLuck) {
     rarity = 'SECRET';
-  } else if (roll < 2 * (totalLuck / 1.2)) {
+  } else if (roll < 0.5 * totalLuck) {
     rarity = 'MYTHIC';
-  } else if (roll < 6 * (totalLuck / 1.1)) {
+  } else if (roll < 2.5 * totalLuck) {
     rarity = 'LEGENDARY';
-  } else if (roll < 15 * totalLuck) {
+  } else if (roll < 8 * totalLuck) {
     rarity = 'EPIC';
-  } else if (roll < 35 * totalLuck) {
+  } else if (roll < 22 * totalLuck) {
     rarity = 'RARE';
-  } else if (roll < 65 * totalLuck) {
+  } else if (roll < 50 * totalLuck) {
     rarity = 'UNCOMMON';
   }
 
@@ -124,7 +128,6 @@ function getRandomCatch(potionBuff = 'normal', rodId = 'training') {
   let fishName = baseFish.name;
   let mutationApplied = null;
 
-  // 15% peluang kena Mutasi
   if (Math.random() < 0.15) {
     const mutation = mutations[Math.floor(Math.random() * mutations.length)];
     fishName = `${mutation.prefix} ${baseFish.name}`;
@@ -150,3 +153,4 @@ module.exports = {
   fishPool,
   getRandomCatch
 };
+    
