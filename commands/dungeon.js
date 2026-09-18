ce21648b5affe62b5f65caefc3b42131baad6cec
raaps199-netizen <raaps199@gmail.com>
2026-09-18 17:52:54 +0700

    Update dungeon.js

--- commands/dungeon.js
+++ commands/dungeon.js
@@ -117,7 +117,7 @@ async function handleDungeonCommand(sock, msg, args) {
 
     const completion = await groq.chat.completions.create({
       messages: [{ role: "user", content: systemPrompt }],
-      model: "llama-3.3-70b-versatile",
+      model: llama3-70b-8192",
       temperature: 0.3
     });
 
